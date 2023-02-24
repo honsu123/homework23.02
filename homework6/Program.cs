02c21cbe3807595d7c1c09e8c85c2bddcae64485
kirill <nicknamekirill@gmail.com>
2023-02-24 17:11:34 +0300

    homework6_done

--- homework6/Program.cs
+++ homework6/Program.cs
@@ -1,11 +1,11 @@
-﻿// Console.WriteLine("Напишите Число");
-// int number = Convert.ToInt32(Console.ReadLine());
-// int result = number / 2;
-// if (result==0); 
-// {
-//     Console.WriteLine("Да");
-// }
-// else 
-// {
-//     Console.WriteLine("Нет");
-// }
+﻿Console.WriteLine("Напишите Число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 2;
+if (result == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
