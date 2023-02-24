c20160f043a3983bc16095e6e58e0fd834c91915
kirill <nicknamekirill@gmail.com>
2023-02-24 17:19:30 +0300

    homework8_done

--- homework8/Program.cs
+++ homework8/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Напишите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int step = 2;
+if (number>1);
+{
+    while(step <= number)
+    {
+        Console.WriteLine(step + " ");
+        step = step + 2;
+    }
+}
