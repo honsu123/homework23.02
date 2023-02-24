99a81416a92bc5f9f7957ab861d4e672fc2e0b96
kirill <nicknamekirill@gmail.com>
2023-02-24 16:57:02 +0300

    homework4_done

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("Напишите три числа");
+int first_number = Convert.ToInt32(Console.ReadLine());
+int second_number = Convert.ToInt32(Console.ReadLine());
+int third_number = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (first_number>second_number) max = first_number;
+else max = second_number;
+if (third_number>max) max = third_number;
+Console.WriteLine(max);
