1668f2bf3b35896532d0d2e6477f42ee877d259f
kirill <nicknamekirill@gmail.com>
2023-03-01 03:13:21 +0300

    end

--- homework13/Program.cs
+++ homework13/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 1000)
+{
+    while(number > 999)
+   {
+    number /=10;
+   }
+    Console.WriteLine(number % 10);
+}
+else if (number < 99) 
+    Console.WriteLine("Третьей цифры нет");
+else
+    Console.WriteLine(number % 10);
