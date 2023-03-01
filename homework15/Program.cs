5ed0ccd2e60e50439352059fbf0d4603026ac6b5
kirill <nicknamekirill@gmail.com>
2023-03-01 03:29:16 +0300

    week

---
+++ homework15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Напишите цифру");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>0 && number<8)
+    if  (number>5)
+        Console.WriteLine("Это выходной");
+    else
+        Console.WriteLine("Это не выходной");
+else
+    Console.WriteLine("день не подходит");
